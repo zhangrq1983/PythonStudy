a5a68ec5402fd6d094cadfa4b163385d856b1280
zhangrq <zhangrongianglo@163.com>
2020-04-19 22:26:48 +0800

    update

--- lesson_02_Python入门/code/01.基本概念.py
+++ lesson_02_Python入门/code/01.基本概念.py
@@ -1,9 +1,12 @@
-print('Hello World')
+print('Hello World', 'haha')
 print(10+5)
 print("abc")
-
 a = 10
 print(a)
+print(max(1, 2, 3))
+
+print('HelloWorldHelloWorldHelloWorldHelloWorldHelloWorldHelloWorldHelloWorldHelloWorldHelloWorldHelloWorldHelloWorld\
+123HelloWorld')
 
 # print('hello','哈哈','abc')
 # print(123)
